d8b0e20968c9f046911ffd7a35f33f557cbd9882
fouladgardev <mohammadfouladgarphp@gmail.com>
2018-04-18 16:09:45 +0430

    removal of unused methods

--- src/Fouladgar/Filter/FilterServiceProvider.php
+++ src/Fouladgar/Filter/FilterServiceProvider.php
@@ -5,23 +5,4 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class FilterServiceProvider extends BaseServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 }
